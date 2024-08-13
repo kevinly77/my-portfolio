2affd212a90f154c2c91b7f27c5f7538249da58c
Kevin Ly <44701340+kevinly77@users.noreply.github.com>
2024-08-13 03:01:53 -0400

    Add: card sections

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,5 +1,82 @@
-import React from "react";
+import stylex from "@stylexjs/stylex";
+const styles = stylex.create({
+  wrapper: {
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: "1rem",
+  },
 
+  middlePill: {
+    //display: "flex",
+    position: "relative",
+    justifyContent: "center",
+    alignItems: "center",
+    //width: "400px",
+    //gap: "2rem",
+    //border: "2px solid #B00580",
+    //padding: "1rem",
+    //boxShadow: "0px 4px 25px #B00580",
+    backgroundColor: "rgba(176, 5, 128, 0.1)",
+    boxSizing: "border-box",
+    clipPath: "polygon(0 20%, 10% 0, 100% 0, 100% 80%, 90% 100%, 0% 100%)",
+
+    /* "::before": {
+      content: "",
+      width: "100%",
+      height: "100%",
+      position: "absolute",
+      backdropFilter: "blur(10px)",
+      backgroundColor: "#B00580",
+      clipPath: "polygon(0 20%, 10% 0, 100% 0, 100% 80%, 90% 100%, 0% 100%)",
+    }, */
+  },
+
+  link: {
+    padding: 0,
+    margin: 0,
+    fontFamily: "var(--font-accent)",
+    height: "100%",
+    width: "100%",
+    color: "var(--cyan-main)",
+  },
+
+  linkList: {
+    display: "flex",
+    padding: "1rem",
+    //height: "100%",
+    margin: 0,
+    gap: "1rem",
+    border: "1px solid #B00580",
+    "::before": {
+      borderRight: "1px solid transparent",
+      borderTop: "1px solid #B00580",
+      content: "",
+      position: "absolute",
+      left: 0,
+      top: 0,
+    },
+  },
+  borderSpan: {
+    position: "absolute",
+    width: "380px",
+    height: "380px",
+    backgroundColor: "#B00580",
+    clipPath: "polygon(0 20%, 10% 0, 100% 0, 100% 80%, 90% 100%, 0% 100%)",
+  },
+});
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div {...stylex.props(styles.wrapper)}>
+      <div {...stylex.props(styles.middlePill)}>
+        <div {...stylex.props(styles.linkList)}>
+          <p {...stylex.props(styles.link)}>Home</p>
+          <p {...stylex.props(styles.link)}>Projects</p>
+          <p {...stylex.props(styles.link)}>Skills</p>
+          <p {...stylex.props(styles.link)}>Contacts</p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,11 @@
 import stylex from "@stylexjs/stylex";
-import React, { useEffect, useRef, useState } from "react";
-import Circles from "../../assets/lots-of-circles.svg";
-import CutBox from "../../assets/cut-box.svg";
+import { useEffect, useRef, useState } from "react";
+import Circles from "/lots-of-circles.svg";
+import BodyIcon from "/body-icon.svg";
+import IntelligenceIcon from "/intelligence-icon.svg";
+import ReflexIcon from "/reflex-icon.svg";
+import TechnicalIcon from "/technical-icon.svg";
+import CoolIcon from "/cool-icon.svg";
 const styles = stylex.create({
   container: {
     backgroundColor: "black",
@@ -11,31 +15,33 @@ const styles = stylex.create({
     alignItems: "center",
     paddingInline: "2rem",
     gap: "2rem",
+    boxSizing: "border-box",
   },
   flag: {
     display: "flex",
-    border: "4px solid #B00580",
+    border: "4px solid var(--purple-main)",
     padding: "1rem",
-    boxShadow: "0px 4px 25px #B00580",
+    boxShadow: "0px 4px 25px var(--purple-main)",
+    backgroundColor: "rgba(176, 5, 128, 0.1)",
     //padding: "5rem",
   },
 
   sidewaysName: {
-    color: "#B00580",
-    fontFamily: "Zen Dots",
+    color: "var(--purple-main)",
+    fontFamily: "var(--font-main)",
     whiteSpace: "nowrap",
     writingMode: "vertical-lr",
     padding: "0",
     margin: "0",
     fontSize: "5em",
-    //textShadow: "0px 4px 25px #B00580",
+    //textShadow: "0px 4px 25px var(--purple-main)",
   },
 
   sidewaysJapanese: {
     WebkitTextStrokeWidth: "2px",
-    WebkitTextStrokeColor: "#B00580",
-    //textShadow: "0px 4px 25px #B00580",
-    fontFamily: "Zen Dots",
+    WebkitTextStrokeColor: "var(--purple-main)",
+    //textShadow: "0px 4px 25px var(--purple-main)",
+    fontFamily: "var(--font-main)",
     whiteSpace: "nowrap",
     writingMode: "vertical-lr",
     padding: "0",
@@ -52,51 +58,88 @@ const styles = stylex.create({
   },
 
   rightSide: {
-    color: "white",
+    //color: "white",
+    boxSizing: "border-box",
+    display: "flex",
+    flexDirection: "column",
+    //backgroundColor: "red",
+    height: "100%",
+    width: "100%",
   },
 
   description: {
     color: "white",
     fontFamily: "Rajdhani, sans-serif",
-    fontSize: "2em",
-    margin: "0",
-    padding: "0",
+    fontSize: "1.5em",
+    maxWidth: "45rem",
   },
 
   descriptionWrapper: {
-    padding: "1rem",
-    border: "1px solid #B00580",
-    backgroundImage: "url(../../assets/cut-box.svg)", // SVG as background image
-    backgroundSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    boxSizing: "border-box",
+    display: "flex",
+    padding: "2rem",
+    //boxSizing: "border-box",
+    //border: "4px solid transparent",
+    borderStyle: "solid",
+    borderWidth: "0px 20px",
+    borderImage: "url(/cut-box.svg) 0 20 fill",
+
+    //backgroundColor: "blue",
+    //backgroundPosition: "center",
+  },
+
+  svg: {
+    width: "100%",
+    height: "100%",
+  },
+
+  infoCard: {
+    display: "flex",
+    //padding: "2rem",
+    //boxSizing: "border-box",
+    //border: "4px solid transparent",
+    borderStyle: "solid",
+    borderWidth: "0px 10px",
+    borderImage: "url(/info-card.svg) 0 0 fill",
+    //backgroundColor: "pink",
+    height: "fit-content",
+    width: "fit-content",
+    paddingTop: "1rem",
+    paddingBottom: "1rem",
+  },
+
+  infoCardTextWrapper: {
+    //backgroundColor: "blue",
+    color: "var(--purple-main)",
+    width: "80%",
+    marginInline: "auto",
+  },
+
+  infoCardHeader: {
+    fontFamily: "var(--font-accent)",
+    color: "var(--purple-main)",
+    fontSize: "1.5em",
+  },
+
+  infoCardDescription: {
+    fontFamily: "var(--font-accent)",
+    color: "var(--cyan-main)",
+  },
+  infoCardAttributes: {
+    display: "flex",
+    flexDirection: "column",
+    //gap: "0.25rem",
+    paddingLeft: "1rem",
+  },
+  infoCardAttribute: {
+    display: "flex",
+    gap: "0.5rem",
+  },
+  divider: {
+    border: "1px solid var(--purple-main)",
+    width: "100%",
   },
 });
-("../../assets/cut-box.svg");
 export default function HomePage() {
-  const rotatedH1Ref = useRef<HTMLHeadingElement | null>(null);
-  const [containerSize, setContainerSize] = useState<{
-    width: number;
-    height: number;
-  }>({
-    width: 0,
-    height: 0,
-  });
-
-  useEffect(() => {
-    const updateContainerSize = () => {
-      if (rotatedH1Ref.current) {
-        const boundingBox = rotatedH1Ref.current.getBoundingClientRect();
-        setContainerSize({
-          width: boundingBox.width,
-          height: boundingBox.height,
-        });
-      }
-    };
-    updateContainerSize();
-    window.addEventListener("resize", updateContainerSize);
-  }, []);
   return (
     <div {...stylex.props(styles.container)}>
       <div {...stylex.props(styles.leftSide)}>
@@ -109,17 +152,73 @@ export default function HomePage() {
       </div>
 
       <div {...stylex.props(styles.rightSide)}>
-        <div {...stylex.props(styles.descriptionWrapper)}>
+        <div {...stylex.props(styles.infoCard)}>
+          <div {...stylex.props(styles.infoCardTextWrapper)}>
+            <div>
+              <p {...stylex.props(styles.infoCardHeader)}>NAME</p>
+              <hr {...stylex.props(styles.divider)} />
+              <p {...stylex.props(styles.infoCardDescription)}>Kevin Ly</p>
+            </div>
+            <div>
+              <p {...stylex.props(styles.infoCardHeader)}>OCCUPATION</p>
+              <hr {...stylex.props(styles.divider)} />
+              <p {...stylex.props(styles.infoCardDescription)}>
+                Software Developer / Netrunner
+              </p>
+            </div>
+            <div>
+              <p {...stylex.props(styles.infoCardHeader)}>BACK STORY</p>
+              <hr {...stylex.props(styles.divider)} />
+              <p {...stylex.props(styles.infoCardDescription)}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec
+                purus ac libero finibus euismod. Sed blandit, nunc at tincidunt
+                tincidunt, nunc velit convallis nulla, id aliquet arcu tortor
+                vitae erat.
+              </p>
+            </div>
+            <div>
+              <p {...stylex.props(styles.infoCardHeader)}>
+                ATTRIBUTES OF CAPABILITY PREDISPOSITIONS
+              </p>
+              <hr {...stylex.props(styles.divider)} />
+              <div {...stylex.props(styles.infoCardAttributes)}>
+                <div {...stylex.props(styles.infoCardAttribute)}>
+                  <img src={BodyIcon} />
+                  <p>5</p>
+                  <p>BODY</p>
+                </div>
+                <div {...stylex.props(styles.infoCardAttribute)}>
+                  <img src={BodyIcon} />
+                  <p>5</p>
+                  <p>BODY</p>
+                </div>
+                <div {...stylex.props(styles.infoCardAttribute)}>
+                  <img src={BodyIcon} />
+                  <p>5</p>
+                  <p>BODY</p>
+                </div>
+                <div {...stylex.props(styles.infoCardAttribute)}>
+                  <img src={BodyIcon} />
+                  <p>5</p>
+                  <p>BODY</p>
+                </div>
+                <div {...stylex.props(styles.infoCardAttribute)}>
+                  <img src={BodyIcon} />
+                  <p>5</p>
+                  <p>BODY</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div {...stylex.props(styles.descriptionWrapper)}>
           <p {...stylex.props(styles.description)}>
             Hey I’m Kevin and I like to code. I have a lot of experience doing
             it. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Duis aute irure dolor in
-            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-            pariatur.
+            ad minim venian.
           </p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
